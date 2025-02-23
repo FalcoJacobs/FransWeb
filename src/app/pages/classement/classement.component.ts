fa350675dab5730833afca5a88e583c16bbd4f0c
Falco <Falcojdb@gmail.com>
2025-02-23 17:09:36 +0100

    created classement layout and routerlinks fixed keydown to keyup on inputs in conjugaison and vocabulaire tests fixed image on homepage

--- src/app/pages/classement/classement.component.ts
+++ src/app/pages/classement/classement.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-classement',
-  imports: [],
+  imports: [RouterLink, CommonModule, NgFor],
   templateUrl: './classement.component.html',
   styleUrl: './classement.component.scss'
 })
 export class ClassementComponent {
+  selected: string = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.selected = params['selected'];
+    });
+    console.log(this.selected);    
+  }
 }
